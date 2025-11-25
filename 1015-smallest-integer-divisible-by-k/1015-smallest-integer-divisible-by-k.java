f74c0f784579b6e49e518b0f6b0e65834c9cf55f
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2025-11-25 16:02:01 +0530

    Time: 2 ms (97.52%), Space: 42.2 MB (27.56%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -1,6 +1,7 @@
 class Solution {
     public int smallestRepunitDivByK(int k) {
         int sum=0,c=0;
+        if(k%2==0 || k%5==0)  return -1;
         for(int i=1;i<=k;i++){
             sum=(sum*10+1)%k;
             if(sum==0){
