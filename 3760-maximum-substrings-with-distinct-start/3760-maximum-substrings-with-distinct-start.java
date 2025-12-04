8c3afc460516014818180480d66c57008e635c4d
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2025-12-04 22:44:40 +0530

    Delete 3760-maximum-substrings-with-distinct-start directory

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int maxDistinct(String s) {
-        HashSet<Character> set=new HashSet<>();
-        for(char ch:s.toCharArray()){
-            set.add(ch);
-        }
-        return set.size();
-    }
-}
