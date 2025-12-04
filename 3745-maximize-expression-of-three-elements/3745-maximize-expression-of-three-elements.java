ef24cb0018ddbcb698541dacc8f0ab3948aadc50
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2025-12-04 22:42:52 +0530

    Delete 3745-maximize-expression-of-three-elements directory

--- 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int maximizeExpressionOfThree(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        return nums[n-1]+nums[n-2]-nums[0];
-    }
-}
