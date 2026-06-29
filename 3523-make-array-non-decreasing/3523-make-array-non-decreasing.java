19df4185b6d5686735eccbee4fb7381778abdd09
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2026-06-29 17:37:37 +0530

    Time: 2 ms (100%), Space: 137.4 MB (80%) - LeetHub

--- 3523-make-array-non-decreasing/3523-make-array-non-decreasing.java
+++ 3523-make-array-non-decreasing/3523-make-array-non-decreasing.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumPossibleSize(int[] nums) {
-        int size=0,prev=-1;
+        int size=0,prev=0;
         for(int num : nums){
             if(num>=prev){
                 prev=num;
