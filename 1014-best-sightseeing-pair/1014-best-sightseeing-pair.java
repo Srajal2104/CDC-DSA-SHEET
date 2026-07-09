5c7d17283a8b4a17af2cd2ad9418d3e00c84d67e
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2026-07-09 17:51:51 +0530

    Time: 4 ms (89.85%), Space: 54 MB (84.77%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int res=0;
+        int cur=0;
+        for(int i : values){
+            res=Math.max(res,cur+i);
+            cur=Math.max(cur,i)-1;
+        }
+        return res;
+    }
+}
