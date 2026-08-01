f86cc5b6a3a850b0d9d038539a89518003812f28
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2026-08-02 00:31:04 +0530

    Time: 1 ms (100%), Space: 44 MB (42.11%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int c0=0,c1=0;
+        int ans=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='0'){
+                c0++;
+            }
+            else{
+                c1++;
+            }
+            if(Math.abs(c0-c1)<=1){
+              ans++;
+            }
+        }
+        return ans;
+    }
+}
