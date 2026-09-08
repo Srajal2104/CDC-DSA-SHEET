009edf993f7b08fdb5eb3fa9112192b02586e348
Srajal Sahu <144127635+Srajal2104@users.noreply.github.com>
2026-09-08 18:04:28 +0530

    Time: 0 ms (100%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,10 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int c=0;
-        while(n>=1000){
-            c++;
-            n--;
-        }
-        return c;
+        return Math.max(0,n-999);
     }
 }
